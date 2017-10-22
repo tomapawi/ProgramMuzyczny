2ca46bbb144ad3912e1f5f803e3ab40447eac95b
tomapawi <32064441+tomapawi@users.noreply.github.com>
2017-10-22 18:51:53 +0200

    Add files via upload

---
+++ Muzyka/Muzyka/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Muzyka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "All files|*.*" })
+            {
+                if(ofd.ShowDialog()==DialogResult.OK)
+                {
+                    List<MediaFile> files = new List<MediaFile>();
+                    foreach(string fileName in ofd.FileNames)
+                    {
+                        FileInfo fi= new FileInfo(fileName);
+                        files.Add(new MediaFile() {FileName=Path.GetFileNameWithoutExtension(fi.FullName),Path=fi.FullName});
+                    }
+                    listBox1.DataSource=files;
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = listBox1.SelectedItem as MediaFile;
+            if (file != null)
+            {
+                axWindowsMediaPlayer1.URL = file.Path;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.ValueMember = "Path";
+            listBox1.DisplayMember = "FileName";
+        }
+    }
+}
---
+++ Muzyka/Muzyka/MediaFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Muzyka
+{
+    public class MediaFile
+    {
+        public string FileName { get; set; }
+
+        public string Path { get; set; }
+    }
+}
